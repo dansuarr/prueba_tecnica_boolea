388f85aa954bcf523c647a620dc4a2b14daec355
danielsuarez <danisrodriguez2001@gmail.com>
2025-08-15 17:12:50 +0200

    Problema 2 resuelto

--- problema_2.py
+++ problema_2.py
@@ -1,17 +1,18 @@
-a = 123
+numero = int(input('Introduce un número entero positivo: '))
 
 
-def tiene_digitos_unicos(numero: int) -> bool:
-    numero_str = str(numero)
-    numeros_distintos = set(numero_str)
-    len_numero = len(numero_str)
-    len_numeros_distintos = len(numeros_distintos)
+def tiene_digitos_unicos(n: int) -> bool:
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('El número debe ser entero positivo.')
 
-    if len_numero == len_numeros_distintos:
-        return True
-    else:
-        return False
+    n_str = str(n)
+    n_distintos = set(n_str)
+    return len(n_str) == len(n_distintos)
 
 
-b = tiene_digitos_unicos(a)
-print(b)
+resultado = tiene_digitos_unicos(numero)
+
+if resultado:
+    print(f'Todos los dígitos del número {numero} son diferentes.')
+else:
+    print(f'El número {numero} tiene dígitos repetidos.')
