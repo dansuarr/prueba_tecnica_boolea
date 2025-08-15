e5fba151bf0903d94fa42b38b667530938d5f05d
danielsuarez <danisrodriguez2001@gmail.com>
2025-08-15 17:22:27 +0200

    Problema 6 resuelto

--- problema_6.py
+++ problema_6.py
@@ -1,14 +1,15 @@
 from typing import List
 
-lista = [1, 1, 0]
+lista = input('Introduce una lista de ceros y unos separados por comas: ')
+lista = [int(x) for x in lista.split(',')]
 
 
 def ordenar_ceros_y_unos(lista: List[int]) -> List[int]:
     numeros_validos = {0, 1}
     if not set(lista).issubset(numeros_validos):
-        raise ValueError('La lista debe contener solo 0 y 1.')
+        raise ValueError('La lista debe contener solo ceros y unos.')
     return sorted(lista)
 
 
-b = ordenar_ceros_y_unos(lista)
-print(b)
+resultado = ordenar_ceros_y_unos(lista)
+print(f'Lista ordenada: {resultado}')
