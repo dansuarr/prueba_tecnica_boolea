9d47f8348de51b232dc0172e8c157c02a7ba55a9
danielsuarez <danisrodriguez2001@gmail.com>
2025-08-15 18:08:40 +0200

    Problema 1 resuelto

--- problema_1.py
+++ problema_1.py
@@ -1,7 +1,34 @@
-a = 0
+numero = int(input('Introduce un número entero positivo: '))
 
-def es_primo_circular(n: int) -> bool:
-    if n < 2:
+if numero <= 0:
+    raise ValueError('El número debe ser entero positivo.')
+
+
+def es_primo(n: int) -> bool:
+    if n == 2:
+        return True
+    if n < 2 or n % 2 == 0:
         return False
 
-es_primo_circular(a)
+    # Probamos a dividir n entre todos los números enteros entre 3 y raiz de n.
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def es_primo_circular(n: int) -> bool:
+    n_str = str(n)
+    for i in range(len(n_str)):
+        n_rotado = int(n_str[i:] + n_str[:i])
+        if not es_primo(n_rotado):
+            return False
+    return True
+
+
+resultado = es_primo_circular(numero)
+
+if resultado:
+    print(f'El número {numero} es primo circular.')
+else:
+    print(f'El número {numero} no es primo circular.')
